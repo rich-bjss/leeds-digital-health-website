8ac5ace6536bac1f800e66cdf62e8905bc1f6c14
Sam Widdowson <sam.widdowson@bjss.com>
2024-01-19 12:32:08 +0000

    008 - add sponsor page

---
+++ app/committee/page.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+
+import backgroundImage from "@/public/TMP_FALLBACK_GRAPHIC.jpg"
+import ldhCommitteeImage from "@/public/LDHCommittee.png"
+
+export default function SponsorPage() {
+    return <section id="committee" className="h-screen w-screen">
+        <div className="fixed h-screen w-screen overflow-hidden -z-30">
+            <Image
+             src={backgroundImage}
+              fill 
+              alt="Tech background image"
+              style={{objectFit: "cover"}}
+               />
+        </div>
+        <div className="h-screen w-screen -z-20 fixed bg-gradient-to-b from-white">
+            <div className="text-center"></div>
+        </div>
+        <div className="fixed top-5 h-screen w-screen -z-10">
+            <Image
+                src={ldhCommitteeImage}
+                fill
+                alt="LDH Committee Member Organisations"
+                style={{ objectFit: "scale-down" }}
+            />
+            </div>
+    </section>
+}
--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -5,30 +5,31 @@ import MenuButton from "./menu-button"
 import LinkButton from "../ui-elements/link-button"
 
 export default function Header() {
-    return (
-      <nav className="bg-white fixed w-full h-22 z-50" role="navigation">
-        <div className="container mx-auto px-4 py-5 flex flex-wrap items-center md:flex-no-wrap">
-          <div className="mr-4 md:mr-8">
-            <Link href="/">
-              <Image
-                priority
-                className="w-40 h-auto"
-                width="160"
-                height="120"
-                src="/logo.svg"
-                alt="Leeds Digital Health Logo"
-              />
-            </Link>
-          </div>
-          <div className="mx-16">
-            <LinkButton href="/#upcoming">Upcoming Events</LinkButton>
-          </div>
-          <MenuButton />
-          <div className="w-full md:w-auto md:flex-grow md:flex md:items-center">
-            <ul className="flex flex-col mt-4 -mx-4 pt-4 md:flex-row md:items-center md:mx-0 md:mt-0 md:pt-0 md:mr-4 lg:mr-8 border-0"></ul>
-            <ul className="flex flex-col mt-4 -mx-4 pt-4 md:flex-row md:items-center md:mx-0 md:ml-auto md:mt-0 md:pt-0 border-0"></ul>
-          </div>
+  return (
+    <nav className="bg-white fixed w-full h-22 z-50" role="navigation">
+      <div className="container mx-auto px-4 py-5 flex flex-wrap items-center md:flex-no-wrap">
+        <div className="mr-4 md:mr-8">
+          <Link href="/">
+            <Image
+              priority
+              className="w-40 h-auto"
+              width="160"
+              height="120"
+              src="/logo.svg"
+              alt="Leeds Digital Health Logo"
+            />
+          </Link>
         </div>
-      </nav>
-    )
-  }
+        <MenuButton />
+        <div className="w-full md:w-auto md:flex md:flex md:items-center">
+          <ul className="flex flex-col mt-4 -mx-4 pt-4 md:flex-row md:items-right md:mx-0 md:mt-0 md:pt-0 md:mr-4 lg:mr-8 border-0">
+            <LinkButton href="/#upcoming">Upcoming Events</LinkButton>
+          </ul>
+          <ul className="flex flex-col mt-4 -mx-4 pt-4 md:flex-row md:items-center md:mx-0 md:ml-auto md:mt-0 md:pt-0 border-0">
+            <LinkButton href="/committee">Making it Possible</LinkButton>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
