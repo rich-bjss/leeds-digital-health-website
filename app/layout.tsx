f240c7857ec9150d86402f222f2052542de64620
Sam Widdowson <sam.widdowson@bjss.com>
2024-01-19 10:56:50 +0000

    Make nav bar fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import "./globals.css"
 
 import { Inter } from "next/font/google"
 
-import Header from "@/components/header"
-import Footer from "@/components/footer"
+import Header from "@/components/layout/header"
+import Footer from "@/components/layout/footer"
 
 export const metadata = {
   title: `Leeds Digital Health`,
@@ -25,7 +25,7 @@ export default function RootLayout({
     <html lang="en" className={inter.variable}>
       <body>
         <Header />
-        <section className="min-h-screen">
+        <section className="min-h-screen pt-24">
           <main>{children}</main>
           <Footer />
         </section>
--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image"
 import Link from "next/link"
+import MenuButton from "./menu-button"
 
-import LinkButton from "./ui-elements/link-button"
+import LinkButton from "../ui-elements/link-button"
 
 export default function Header() {
     return (
-      <nav className="bg-white" role="navigation">
-        <div className="container mx-auto p-4 flex flex-wrap items-center md:flex-no-wrap">
+      <nav className="bg-white fixed w-full h-22 z-50" role="navigation">
+        <div className="container mx-auto px-4 py-5 flex flex-wrap items-center md:flex-no-wrap">
           <div className="mr-4 md:mr-8">
             <Link href="/">
               <Image
@@ -22,21 +23,7 @@ export default function Header() {
           <div className="mx-16">
             <LinkButton href="/#upcoming">Upcoming Events</LinkButton>
           </div>
-          <div className="ml-auto hidden">
-            <button
-              className="flex items-center px-3 py-2 border rounded"
-              type="button"
-            >
-              <svg
-                className="h-3 w-3"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <title>Menu</title>
-                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-              </svg>
-            </button>
-          </div>
+          <MenuButton />
           <div className="w-full md:w-auto md:flex-grow md:flex md:items-center">
             <ul className="flex flex-col mt-4 -mx-4 pt-4 md:flex-row md:items-center md:mx-0 md:mt-0 md:pt-0 md:mr-4 lg:mr-8 border-0"></ul>
             <ul className="flex flex-col mt-4 -mx-4 pt-4 md:flex-row md:items-center md:mx-0 md:ml-auto md:mt-0 md:pt-0 border-0"></ul>
