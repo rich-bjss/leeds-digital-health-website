7d4862ded8e64e31717ddcd74269a995e22b930e
Sam Widdowson <sam.widdowson@bjss.com>
2024-01-30 14:46:40 +0000

    Remove debug output

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -9,8 +9,6 @@ import { getPostBySlug } from "@/lib/api/posts"
 async function DisplayPost({ slug }: { slug: string }) {
   const post = await getPostBySlug(slug)
 
-  console.log(post)
-
   return (
     <div>
       <Link href="/news" className="text-pink py-4 px-4 rounded-t-lg hover:bg-slate-50">&larr; Back to news feed</Link>
