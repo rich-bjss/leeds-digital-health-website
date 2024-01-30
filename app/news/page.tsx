26f05f304c3f2f8892c0e90c4f8d07db4c259402
Sam Widdowson <sam.widdowson@bjss.com>
2024-01-30 13:07:56 +0000

    Display a message when no news posts were found

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import { Suspense } from "react"
 
 import News from "../../components/news/news"
@@ -7,7 +8,20 @@ import { getNewsPosts } from "@/lib/api/posts"
 
 async function DisplayNews() {
   const posts = await getNewsPosts()
-  return <News morePosts={posts!} />
+  if (!posts || posts.length === 0)
+    return (
+      <div>
+        <h2 className="mb-8 text-6xl ml-2 md:text-6xl font-bold tracking-tighter leading-tight text-navy">
+          News
+        </h2>
+        <div className="">
+          <p className="text-lg font-semibold italic ml-2 mb-6">There are no news items to display.</p>
+          <Link href="/" className="text-pink py-4 px-4 rounded-lg mt-8 bg-white hover:bg-slate-50">&larr; Back to main page</Link>
+        </div>
+      </div>
+    )
+
+  return <News morePosts={posts} />
 }
 
 export default function NewsPage() {
