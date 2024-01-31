71bca2c4a958d12d1c2de844b5976cd1ad5ab904
Sam Widdowson <sam.widdowson@bjss.com>
2024-01-31 13:54:05 +0000

    Styling changes to header nav links

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -16,7 +16,7 @@ export function NavLink({
       href={href}
       className={cn(
         "text-xs px-4 py-2 sm:text-lg font-bold bg-white text-navy",
-        "hover:text-blue-700 hover:border-b-2 border-blue-700"
+        "hover:text-pink hover:border-b-2 border-pink"
       )}
     >
       {children}
@@ -27,30 +27,30 @@ export function NavLink({
 export default function Header() {
   return (
     <nav className="bg-white fixed h-22 z-50 w-full" role="navigation">
-      <div className="container mx-auto py-2 px-2 sm:py-4 flex flex-wrap items-center">
-        <div className="mr-12 w-full sm:w-max flex justify-center">
-          <Link href="/">
-            <Image
-              priority
-              className="w-40 h-auto"
-              width="160"
-              height="120"
-              src="/logo.svg"
-              alt="Leeds Digital Health Logo"
-            />
-          </Link>
+      <div className="container py-2 px-2 sm:py-4 items-center sm:grid sm:grid-cols-2 ml-8 w-full">
+        <div className="">
+          <div className="mr-12 w-full ml-4 sm:w-max">
+            <Link href="/">
+              <Image
+                priority
+                className="w-40 h-auto"
+                width="160"
+                height="120"
+                src="/logo.svg"
+                alt="Leeds Digital Health Logo"
+              />
+            </Link>
+          </div>
+          <MenuButton />
         </div>
-        <MenuButton />
-        <div className="w-full sm:w-max">
-          <ul className="w-full flex justify-center mt-4 pt-0 sm:mt-0">
-            <li className="">
-              <NavLink href="/news">News</NavLink>
-            </li>
-            <li className="">
-              <NavLink href="/committee">LDH Committee</NavLink>
-            </li>
-          </ul>
-        </div>
+        <ul className="w-full mt-4 pt-0 sm:mt-0 text-left sm:text-right">
+          <li className="inline-block">
+            <NavLink href="/news">News</NavLink>
+          </li>
+          <li className="inline-block">
+            <NavLink href="/committee">LDH Committee</NavLink>
+          </li>
+        </ul>
       </div>
     </nav>
   )
