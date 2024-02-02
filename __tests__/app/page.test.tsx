3824f35f9bc38003d3ded26df0cef91e5208838f
Sam Widdowson <sam.widdowson@bjss.com>
2024-02-02 16:19:06 +0000

    More helpful/specific homepage unit test

--- __tests__/app/page.test.tsx
+++ __tests__/app/page.test.tsx
@@ -7,9 +7,17 @@ jest.mock('../../components/home/upcoming/upcoming-events')
 jest.mock('../../components/layout/footer')
 
 describe('Page', () => {
-    it('renders the expected home page content', ()=> {
-        const {container} = render(<Page />)
+    it('renders the expected home page content', async ()=> {
+        render(<Page />)
 
-        expect(container).toMatchSnapshot()
+        const footer = screen.queryByTestId('footer-mock')
+        const heroPost = screen.queryByTestId('hero-post-mock')
+        const upcoming = screen.queryByTestId('upcoming-events-mock')
+        const headline = screen.queryByTestId('headline-content-mock')
+
+        expect(footer).toBeInTheDocument()
+        expect(heroPost).toBeInTheDocument()
+        expect(upcoming).toBeInTheDocument()
+        expect(headline).toBeInTheDocument()
     })
 })
